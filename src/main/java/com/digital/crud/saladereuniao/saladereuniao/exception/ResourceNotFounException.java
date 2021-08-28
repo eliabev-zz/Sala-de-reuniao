b013bddecec817c47375ce8c609df4bf306cf9f8
eliabev <eliabev@gmail.com>
2021-08-28 09:35:45 -0300

    Configure exceptions files

--- src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/ResourceNotFounException.java
+++ src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/ResourceNotFounException.java
@@ -1,4 +1,14 @@
 package com.digital.crud.saladereuniao.saladereuniao.exception;
 
-public class ResourceNotFounException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+public class ResourceNotFounException extends Exception {
+
+    private static final long serialVersionUID= 1L;
+    public ResourceNotFounException(String message) {
+        super(message);
+    }
+
 }
